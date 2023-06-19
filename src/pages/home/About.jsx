0189175758cef0e3ec9e08fa12a1aefbb2b43173
hossainarif37 <hossainarif4937@gmail.com>
2023-06-20 02:22:59 +0600

    fix

--- src/pages/home/About.jsx
+++ src/pages/home/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <section className='flex flex-col-reverse lg:flex-row items-center'>
             <img src={doctor} alt="doctor" className='lg:w-[43.5%]' />
-            <div className='text-[#04083A] px-3 lg:pl-4 py-4 lg:py-0 2xl:px-10'>
+            <div className='text-[#04083A] px-3 lg:pl-4 py-4 lg:py-0'>
                 <h1 className='text-7xl font-bold '>About Us</h1>
                 <h3 className='text-2xl py-3 lg:py-5 '>With BillCare, you can pay for airtime, internet data, electricity <br className='hidden lg:block 2xl:hidden' />
                     bills, make flight ticket reservations, and access free medical <br className='hidden lg:block 2xl:hidden' />
