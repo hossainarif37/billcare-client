320f06fb6de585a58b876d002464fb4316f1ed12
hossainarif37 <hossainarif4937@gmail.com>
2023-06-20 02:13:45 +0600

    fix responsiveness

--- src/pages/home/About.jsx
+++ src/pages/home/About.jsx
@@ -6,8 +6,8 @@ const About = () => {
             <img src={doctor} alt="doctor" className='lg:w-[43.5%]' />
             <div className='text-[#04083A] px-3 lg:pl-4 py-4 lg:py-0'>
                 <h1 className='text-7xl font-bold '>About Us</h1>
-                <h3 className='text-2xl py-3 lg:py-5 '>With BillCare, you can pay for airtime, internet data, electricity <br className='hidden lg:block xl:hidden' />
-                    bills, make flight ticket reservations, and access free medical <br className='hidden lg:block xl:hidden' />
+                <h3 className='text-2xl py-3 lg:py-5 '>With BillCare, you can pay for airtime, internet data, electricity <br className='hidden lg:block 2xl:hidden' />
+                    bills, make flight ticket reservations, and access free medical <br className='hidden lg:block 2xl:hidden' />
                     consultations.
                 </h3>
                 <Link to='' className="text-2xl hover:font-semibold hover:after:content-['..']">Read More...</Link>
