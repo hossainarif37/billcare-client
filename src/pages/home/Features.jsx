ce40e863456d878cb6dfe70030215f14488f931d
hossainarif37 <hossainarif4937@gmail.com>
2023-06-20 02:48:40 +0600

    fix

--- src/pages/home/Features.jsx
+++ src/pages/home/Features.jsx
@@ -10,7 +10,7 @@ const Features = () => {
                     </h3>
                 </div>
                 <div>
-                    <h1 className='text-4xl lg:text-6xl font-bold text-[#4D4D4D]'>Pro Membership Benefits</h1>
+                    <h1 className='text-4xl lg:text-6xl font-bold mb-5 text-[#4D4D4D]'>Pro Membership Benefits</h1>
                     <h3 className='text-4xl'>Get exclusive features and benefits as a Pro member,
                         including access to premium customer support.
 
