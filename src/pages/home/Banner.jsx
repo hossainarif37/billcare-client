6407242380d842cd15c8543b9b761404bac0cf52
hossainarif37 <hossainarif4937@gmail.com>
2023-06-20 02:10:03 +0600

    fix some bugs again

--- src/pages/home/Banner.jsx
+++ src/pages/home/Banner.jsx
@@ -3,7 +3,7 @@ import banner from '../../assets/images/banner.jpg'
 const Banner = () => {
     return (
         <div className='relative'>
-            <img src={banner} alt="" />
+            <img className='w-full' src={banner} alt="" />
             <ul className='lg:flex flex-col lg:flex-row hidden items-center absolute top-0 text-[#ddd] space-x-14 pl-[9%] py-10'>
                 <li className='text-2xl hover:text-white'><Link to='' href='#features'>Features</Link></li>
                 <li className='text-2xl hover:text-white'><Link to='' href='#pricing'>Pricing</Link></li>
