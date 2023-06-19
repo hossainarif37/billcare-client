616ace24cbfa15a0540e5058c0ff1581a4074bda
hossainarif37 <hossainarif4937@gmail.com>
2023-06-20 02:40:58 +0600

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="w-[1440px] mx-auto">
+    <div className="max-w-[1440px] mx-auto">
       <Outlet />
     </div>
   );
