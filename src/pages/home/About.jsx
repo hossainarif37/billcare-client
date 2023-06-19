fe2ecfdade34c970d36467a4ef50c597d5a6f28d
hossainarif37 <hossainarif4937@gmail.com>
2023-06-20 01:14:31 +0600

    style modified

--- src/pages/home/About.jsx
+++ src/pages/home/About.jsx
@@ -4,13 +4,13 @@ const About = () => {
     return (
         <section className='flex flex-col-reverse lg:flex-row items-center'>
             <img src={doctor} alt="doctor" className='lg:w-[43.5%]' />
-            <div className='text-[#04083A] pl-4 py-4 lg:py-0'>
+            <div className='text-[#04083A] px-3 lg:pl-4 py-4 lg:py-0'>
                 <h1 className='text-7xl font-bold '>About Us</h1>
-                <h3 className='text-2xl py-3 lg:py-5 '>With BillCare, you can pay for airtime, internet data, electricity <br />
-                    bills, make flight ticket reservations, and access free medical <br />
+                <h3 className='text-2xl py-3 lg:py-5 '>With BillCare, you can pay for airtime, internet data, electricity <br className='hidden lg:block' />
+                    bills, make flight ticket reservations, and access free medical <br className='hidden lg:block' />
                     consultations.
                 </h3>
-                <Link to='' className='text-2xl '>Read More...</Link>
+                <Link to='' className="text-2xl hover:font-semibold hover:after:content-['..']">Read More...</Link>
             </div>
         </section>
     );
