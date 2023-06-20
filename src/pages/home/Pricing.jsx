9b4df068d674dd49666702940e44da414758b387
hossainarif37 <hossainarif4937@gmail.com>
2023-06-20 06:05:23 +0600

    fix some style issue

--- src/pages/home/Pricing.jsx
+++ src/pages/home/Pricing.jsx
@@ -1,17 +1,17 @@
 import boy from '../../assets/images/slide_2_small.png'
 const Pricing = () => {
     return (
-        <section className='flex flex-col-reverse lg:flex-row bg-[#04083A]'>
+        <section className='flex flex-col-reverse lg:flex-row bg-[#04083A] '>
             <div>
                 <img src={boy} alt="" />
             </div>
-            <div>
+            <div className='flex flex-col items-center justify-center'>
                 <div className='text-white px-5 lg:px-20 text-center py-10'>
                     <h1 className='font-black text-4xl lg:text-6xl mb-4'>Pro Membership Pricing</h1>
                     <p className='text-3xl'>Upgrade to Pro membership and enjoy all the exclusive
                         features.</p>
                 </div>
-                <div className='flex flex-col mb-7 lg:mb-0 lg:flex-row justify-center gap-5 lg:gap-20 text-[#04083A]'>
+                <div className='flex flex-col lg:flex-row gap-5 lg:gap-28 items-center   text-[#04083A]'>
                     <div className='text-center bg-[#D6D7DB] w-[250px] mx-auto lg:mx-0  py-10 border-4 rounded-3xl border-white'>
                         <h1 className='text-5xl mb-3 font-black'>Yearly</h1>
                         <h3 className='text-4xl'>19,999</h3>
