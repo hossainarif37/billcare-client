11ea2081027c087cb0773614edf858bc436b5775
hossainarif37 <hossainarif4937@gmail.com>
2023-06-20 18:06:36 +0600

    changed signup btn style

--- src/pages/home/About.jsx
+++ src/pages/home/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
         <section className='flex flex-col-reverse lg:flex-row items-center bg-[#FBFAF8]'>
             <img src={doctor} alt="doctor" className='lg:w-[43.5%]' />
             <div className='text-[#04083A] px-3 lg:pl-4 py-4 lg:py-0'>
-                <h1 className='text-7xl font-bold '>About Us</h1>
+                <h1 className='text-4xl lg:text-7xl font-bold '>About Us</h1>
                 <h3 className='text-2xl py-3 lg:py-5 '>With BillCare, you can pay for airtime, internet data, electricity <br className='hidden lg:block 2xl:hidden' />
                     bills, make flight ticket reservations, and access free medical <br className='hidden lg:block 2xl:hidden' />
                     consultations.
--- src/pages/home/Banner.jsx
+++ src/pages/home/Banner.jsx
@@ -4,7 +4,7 @@ const Banner = () => {
     return (
         <div className='relative'>
             <img className='w-fit' src={banner} alt="" />
-            <Link to='/signup' className='block lg:hidden absolute top-2 left-8 text-white'>Sign Up</Link>
+            <Link to='/signup' className=' block bg-[#04083A] lg:hidden absolute top-1 text-xs right-1 text-white rounded-xl px-2 active:scale-95 py-1'>Sign Up</Link>
             <ul className='lg:flex flex-col lg:flex-row hidden items-center absolute top-0 text-[#ddd] space-x-14 pl-[9%] py-10'>
                 <li className='text-2xl hover:text-white'><Link to='' href='#features'>Features</Link></li>
                 <li className='text-2xl hover:text-white'><Link to='' href='#pricing'>Pricing</Link></li>
